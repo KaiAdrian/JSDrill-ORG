06fc2831838fcdecdf906c33dcca07266b9826a0
KaiAdrian83 <thriller30@gmail.com>
2025-08-29 20:17:10 +0200

    mindFuckd instructions..

--- freCodeCampJS/RPG - Dragon Repeller/script.js
+++ freCodeCampJS/RPG - Dragon Repeller/script.js
@@ -6,3 +6,6 @@ let fighting;
 //
 let monsterHealth;
 let inventory = ["stick"];
+
+// In your role playing game, users will be able to track their stats, buy weapons, and fight monsters. Before you can continue with the interactive JavaScript portion of the game, you need to first create the HTML elements that will display the game information.
+// Create four div elements within your #game element. Give them the following respective id values, in order: stats, controls, monsterStats, and text.
