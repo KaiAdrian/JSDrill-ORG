49f20b435f2f45b830c6a681a64f7141abd734b6
KaiAdrian83 <thriller30@gmail.com>
2025-08-30 13:59:44 +0200

    spani query... body bckg style

--- freCodeCampJS/RPG - Dragon Repeller/script.js
+++ freCodeCampJS/RPG - Dragon Repeller/script.js
@@ -7,5 +7,17 @@ let fighting;
 let monsterHealth;
 let inventory = ["stick"];
 
-// In your role playing game, users will be able to track their stats, buy weapons, and fight monsters. Before you can continue with the interactive JavaScript portion of the game, you need to first create the HTML elements that will display the game information.
-// Create four div elements within your #game element. Give them the following respective id values, in order: stats, controls, monsterStats, and text.
+
+
+
+// One method for finding specific elements in your HTML is using the querySelector() method. The querySelector() method takes a CSS selector as an argument and returns the first element that matches that selector. For example, to find the <h1> element in your HTML, you would write:
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+// We have run into a slight problem. You are trying to query your page for a button element, but your script tag is in the head of your HTML. This means your code runs before the browser has finished reading the HTML, and your document.querySelector() will not see the button - because the browser hasn't processed it yet.
+//...closing </body> tag).
+
+// button1.addEventListener('mouseover', () => {
+//     alert(`kliknu si "${button1.id}"`)
+// })
