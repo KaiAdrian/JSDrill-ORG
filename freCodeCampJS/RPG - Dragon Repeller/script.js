41b2ce652e829db618b0225e28ec71d431fd46b8
KaiAdrian83 <thriller30@gmail.com>
2025-08-31 09:04:53 +0200

    funct() goStore

--- freCodeCampJS/RPG - Dragon Repeller/script.js
+++ freCodeCampJS/RPG - Dragon Repeller/script.js
@@ -7,17 +7,27 @@ let fighting;
 let monsterHealth;
 let inventory = ["stick"];
 
-
-
-
 // One method for finding specific elements in your HTML is using the querySelector() method. The querySelector() method takes a CSS selector as an argument and returns the first element that matches that selector. For example, to find the <h1> element in your HTML, you would write:
 
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+
+const monsterHealthText = document.querySelector("#monsterHealth");
 // We have run into a slight problem. You are trying to query your page for a button element, but your script tag is in the head of your HTML. This means your code runs before the browser has finished reading the HTML, and your document.querySelector() will not see the button - because the browser hasn't processed it yet.
 //...closing </body> tag).
 
 // button1.addEventListener('mouseover', () => {
 //     alert(`kliknu si "${button1.id}"`)
 // })
+
+function goStore() {
+    
+}
