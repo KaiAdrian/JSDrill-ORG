09d718ca7520e9942b5bb8bb86fa679e64e4f207
KaiAdrian83 <thriller30@gmail.com>
2025-09-03 16:24:59 +0200

    btn.onclikck

--- freCodeCampJS/RPG - Dragon Repeller/script.js
+++ freCodeCampJS/RPG - Dragon Repeller/script.js
@@ -29,5 +29,26 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 // })
 
 function goStore() {
-    
+	button1.innerText = "Buy 10 health (10 gold)";
+	button2.innerText = "Buy weapon (30 gold)";
+	button3.innerText = "Go to town square";
+
+	button1.onclick = buyHealth;
+	button2.onclick = buyWeapon;
+	button3.onclick = goTown;
+	text.innerText = "You enter the store.";
 }
+function goCave() {
+	console.log("Going to cave.");
+}
+function fightDragon() {
+	console.log("Fighting dragon.");
+}
+//initialize buttons
+button1.onclick = goStore; //DOT NOTATION NAČIN KLICA (brez oklepajev)
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+ function buyHealth(){};
+ function buyWeapon(){};
+ function goTown(){};
