12d16363e1b5a413e4325cb798cfed9ac13907ae
KaiAdrian83 <thriller30@gmail.com>
2025-09-05 18:17:57 +0200

    iz funkcij metode arr objekta

--- freCodeCampJS/RPG - Dragon Repeller/script.js
+++ freCodeCampJS/RPG - Dragon Repeller/script.js
@@ -32,6 +32,8 @@ const locations = [
 	{
 		name: "town square",
 		"button text": ["Go to store", "Go to cave", "Fight dragon"],
+		"button functions": [goStore, goCave, fightDragon],
+		text:'You are in the town square. You see a sign that says "Store".'
 	},
 ]; //role-play  upd trenutno lokacijo junaka
 
