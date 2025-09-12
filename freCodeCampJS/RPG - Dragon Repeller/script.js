ebbfbcd01acdfc763a604bf85f627a8295e97f36
KaiAdrian83 <thriller30@gmail.com>
2025-09-12 13:07:38 +0200

    weapens arr

--- freCodeCampJS/RPG - Dragon Repeller/script.js
+++ freCodeCampJS/RPG - Dragon Repeller/script.js
@@ -104,5 +104,9 @@ function buyHealth() {
 function buyWeapon() {
 	if (gold>=30) {
 		gold -= 30;
+		currentWeaponIndex++;
+		goldText.innerText = gold;
+		let newWeapon = weapons[currentWeaponIndex];
+		text.innerText = "You now have a new weapon.";
 	}
 }
