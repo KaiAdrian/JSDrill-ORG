e9f8117ae3e8fc0536e4ac830a8ef6b1b306d687
KaiAdrian83 <thriller30@gmail.com>
2025-10-07 09:47:03 +0200

    regex

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -1,3 +1,14 @@
-const calorieCounter = document.getElementById('calorie-counter');
+const calorieCounter = document.getElementById("calorie-counter");
 const budgetNumberInput = document.getElementById("budget");
 const entryDropdown = document.getElementById("entry-dropdown");
+const addEntryButton = document.getElementById("add-entry");
+const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
+// In programming, prefixing a variable with is or has is a common practice to signify that the variable represents a boolean value.
+let isError = false;
+// Later on in the project, you will update the value of isError if the user provides an invalid input.
+
+//convert function
+function cleanInputString(str) {
+	const regex = /\+-/; ////REGEX!!!!
+}
