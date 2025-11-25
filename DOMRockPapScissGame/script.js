73f726cb6d52a4460e6268bca88d1811c08f8569
KaiAdrian83 <thriller30@gmail.com>
2025-11-25 17:14:57 +0100

    first lesson

--- DOMRockPapScissGame/script.js
+++ DOMRockPapScissGame/script.js
@@ -1,5 +1,7 @@
 function getRandomComputerResult() {
-  const options = ["Rock", "Paper", "Scissors"];
-
+	const options = ["Rock", "Paper", "Scissors"];
+  // let randomIndex = Math.floor(Math.random() * options.length + 0);
+  const randomIndex = Math.floor(Math.random() * options.length);
+	return options[randomIndex];
 }
 console.log(getRandomComputerResult());
