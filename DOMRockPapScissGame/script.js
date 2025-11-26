c7f1db561e44282748c31e1362dc4053699b3a6b
KaiAdrian83 <thriller30@gmail.com>
2025-11-26 17:47:43 +0100

    hasPlayerWonTheRound(player, computer)

--- DOMRockPapScissGame/script.js
+++ DOMRockPapScissGame/script.js
@@ -1,7 +1,18 @@
 function getRandomComputerResult() {
 	const options = ["Rock", "Paper", "Scissors"];
-  // let randomIndex = Math.floor(Math.random() * options.length + 0);
-  const randomIndex = Math.floor(Math.random() * options.length);
+	// let randomIndex = Math.floor(Math.random() * options.length + 0);
+	const randomIndex = Math.floor(Math.random() * options.length);
 	return options[randomIndex];
 }
 console.log(getRandomComputerResult());
+
+function hasPlayerWonTheRound(player, computer) {
+  return (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Scissors" && computer === "Paper") ||
+    (player === "Paper" && computer === "Rock")
+  );
+}
+console.log(hasPlayerWonTheRound("Rock", "Scissors"));
+console.log(hasPlayerWonTheRound("Scissors", "Rock"));
+console.log(hasPlayerWonTheRound("Paper", "Rock"));
