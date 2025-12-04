4b993cc05be21cad7cb48f7d5d68615645150c04
KaiAdrian83 <thriller30@gmail.com>
2025-12-04 13:15:09 +0100

    winner 3 points

--- DOMRockPapScissGame/script.js
+++ DOMRockPapScissGame/script.js
@@ -29,17 +29,17 @@ function getRoundResults(userOption) {
 	// 	playerScore++;
 	// 	return `Player wins! ${userOption} beats ${computerResult}`;
 	// } else computerScore++;
-  // return `Computer wins! ${computerResult} beats ${userOption}`;
+	// return `Computer wins! ${computerResult} beats ${userOption}`;
 
-  if (hasPlayerWonTheRound(userOption, computerResult)) {
-    playerScore++;
-    return `Player wins! ${userOption} beats ${computerResult}`;
-  } else if (computerResult === userOption) {
-    return `It's a tie! Both chose ${userOption}`;
-  } else {
-	  computerScore++;
-    return `Computer wins! ${computerResult} beats ${userOption}`;
-  }
+	if (hasPlayerWonTheRound(userOption, computerResult)) {
+		playerScore++;
+		return `Player wins! ${userOption} beats ${computerResult}`;
+	} else if (computerResult === userOption) {
+		return `It's a tie! Both chose ${userOption}`;
+	} else {
+		computerScore++;
+		return `Computer wins! ${computerResult} beats ${userOption}`;
+	}
 }
 // console.log(getRoundResults("Rock"));
 // console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
@@ -48,11 +48,21 @@ function getRoundResults(userOption) {
 const playerScoreSpanElement = document.getElementById("player-score");
 const computerScoreSpanElement = document.getElementById("computer-score");
 const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+const optionsContainer = document.querySelector(".options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
 
 function showResults(userOption) {
-  roundResultsMsg.innerText = getRoundResults(userOption);
+	roundResultsMsg.innerText = getRoundResults(userOption);
 	playerScoreSpanElement.innerText = playerScore;
 	computerScoreSpanElement.innerText = computerScore;
-	
-};
+	if (playerScore === 3 || computerScore === 3) {
+		winnerMsgElement.innerText = `${
+			playerScore === 3 ? "Player" : "Computer"
+		} has won the game!`;
+
+		resetGameBtn.style.display = "block";
+		optionsContainer.style.display = "none";
+	}
+}
 showResults("Rock");
