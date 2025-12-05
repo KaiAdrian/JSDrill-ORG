0e1d8141fb5b9b9dde871409e188d3297bd7bbe6
KaiAdrian83 <thriller30@gmail.com>
2025-12-05 10:10:18 +0100

    less1

---
+++ arrayMethodsfind()forEach()map()join()/script.js
@@ -0,0 +1,6 @@
+// #playlist-songs, #play, and #pause elements with the getElementById() method. Assign them to variables playlistSongs, playButton and pauseButton respectively.
+
+//html elem
+const playlistSongs=document.getElementById("playlist-songs");
+const playButton=document.getElementById("play");
+const pauseButton=document.getElementById("pause");
