2dfec79000b7a95d25805a08a68cde66cb6990ea
KaiAdrian83 <thriller30@gmail.com>
2025-12-22 12:35:35 +0100

    find()

--- arrayMethodsfind()forEach()map()join()/script.js
+++ arrayMethodsfind()forEach()map()join()/script.js
@@ -88,8 +88,13 @@ let userData = {
 	songCurrentTime: 0,
 };
 
-const playSong = id => { 
-
+const playSong = (id) => {
+	const song = userData?.songs.find((song) => song.id === id);
+	audio.src = song.src;
+	audio.title = song.title;
+	if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+		audio.currentTime = 0;
+	}
 };
 
 // const printGreeting = () => {
