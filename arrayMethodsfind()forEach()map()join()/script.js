6e53399ea1f776e7b2ea8362c02ec52a51833f43
KaiAdrian83 <thriller30@gmail.com>
2025-12-24 11:12:43 +0100

    onclick interpolat...const pause =()

--- arrayMethodsfind()forEach()map()join()/script.js
+++ arrayMethodsfind()forEach()map()join()/script.js
@@ -102,6 +102,10 @@ const playSong = (id) => {
 	audio.play();
 };
 
+const pauseSong = () => { 
+	userData.songCurrentTime = audio.currentTime;
+};
+
 // const printGreeting = () => {
 // 	console.log("Hello there!");
 // };
@@ -118,12 +122,12 @@ const renderSongs = (array) => {
 	const songsHTML = array
 		.map((song) => {
 			return `<li id="song-${song.id}" class="playlist-song">
-		<button class="playlist-song-info">
+		<button class="playlist-song-info" onclick="playSong(${song.id})">
 		 <span class="playlist-song-title">${song.title}</span>
 		 <span class="playlist-song-artist">${song.artist}</span>
 		 <span class="playlist-song-duration">${song.duration}</span>
 		</button>
-		<button
+		<button 
   class="playlist-song-delete"
   aria-label="Delete ${song.title}"
 >
@@ -139,7 +143,13 @@ const renderSongs = (array) => {
 / renderSongs(userData?.songs);/////ČEKIRIIIIII!!!!!!
 */
 
-playButton.addEventListener("click", () => { });
+playButton.addEventListener("click", () => {
+	if (userData?.currentSong === null) {
+		playSong(userData?.songs[0].id);
+	} else {
+		playSong(userData?.currentSong.id);
+	}
+});
 
 const sortSongs = () => {
 	userData?.songs.sort((a, b) => {
