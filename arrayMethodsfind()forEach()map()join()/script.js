7e496b87676f41a326cb137290795e6140e6b4e0
KaiAdrian83 <thriller30@gmail.com>
2026-01-09 13:36:32 +0100

    setPlayerDisplay highlightCurrentSong

--- arrayMethodsfind()forEach()map()join()/script.js
+++ arrayMethodsfind()forEach()map()join()/script.js
@@ -99,6 +99,8 @@ const playSong = (id) => {
 	}
 	userData.currentSong = song;
 	playButton.classList.add("playing");
+	highlightCurrentSong();
+	setPlayerDisplay();
 	audio.play();
 };
 const pauseSong = () => {
@@ -126,8 +128,25 @@ const playPreviousSong = () => {
 };
 
 //druge funkcije
+const setPlayerDisplay = () => {
+	const playingSong = document.getElementById("player-song-title");
+	const songArtist = document.getElementById("player-song-artist");
+	const currentTitle = userData?.currentSong?.title;
+	const currentArtist = userData?.currentSong?.artist;
+	playingSong.textContent = currentTitle ? currentTitle : "";
+	songArtist.textContent = currentArtist ? currentArtist : "";
+};
 const highlightCurrentSong = () => {
 	const playlistSongElements = document.querySelectorAll(".playlist-song");
+	const songToHighlight = document.getElementById(
+		`song-${userData?.currentSong?.id}`
+	);
+	playlistSongElements.forEach((songEl) => {
+		songEl.removeAttribute("aria-current");
+	});
+	if (songToHighlight) {
+		songToHighlight.setAttribute("aria-current", "true");
+	}
 };
 //gener. HTML-ja
 const renderSongs = (array) => {
