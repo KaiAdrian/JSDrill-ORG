6739bb971b3490be9e2dfb8c4a4e37edda161f1a
KaiAdrian83 <thriller30@gmail.com>
2026-01-11 12:56:19 +0100

    || pa ?

--- arrayMethodsfind()forEach()map()join()/script.js
+++ arrayMethodsfind()forEach()map()join()/script.js
@@ -169,6 +169,17 @@ const renderSongs = (array) => {
 		.join("");
 	playlistSongs.innerHTML = songsHTML;
 };
+const setPlayButtonAccessibleText = () => {
+	const song = userData?.currentSong || userData?.songs[0];
+	playButton.setAttribute(
+		"aria-label",
+		song?.title ? `Play ${song.title}` : "Play"
+	);
+
+	// 	The setPlayButtonAccessibleText function will set the aria-label attribute to the current song, or to the first song in the playlist. And if the playlist is empty, it sets the aria-label to "Play".
+	// Use the setAttribute method on the playButton element to set an attribute named "aria-label". Using a ternary, set the attribute value to Play ${song.title} or "Play" if song?.title is not available.
+	// Don't forget you need string interpolation here, so you need to use backticks.
+};
 //indeks za funkc.predvajanja
 const getCurrentSongIndex = () => {
 	// const animals = ["dog", "cat", "horse"];
