5b58dd24fd13c31cd88fede976d1fe8d4475327b
KaiAdrian <thriller30@gmail.com>
2026-04-03 18:17:49 +0200

    js link

--- palindromeChecker/script.js
+++ palindromeChecker/script.js
@@ -1,8 +1,20 @@
 const checkBtn = document.getElementById("check-btn");
 const input = document.getElementById("text-input");
+const result = document.getElementById("result");
 
+// checkBtn.addEventListener("click", () => {
+//   if ((input.value.trim() === "")) {
+//     alert("Please input a value");
+//   }
+// });
 checkBtn.addEventListener("click", () => {
-  if ((input.value.trim() === "")) {
+  if (input.value.trim() === "") {
     alert("Please input a value");
+    console.log("Please input a value");
+  }
+   else {
+    const cleaned = input.value.toLowerCase();
+    // result.innerText=`${cleaned} is a palindrome `
+    result.innerText=`${input.value} is a palindrome `
   }
 });
