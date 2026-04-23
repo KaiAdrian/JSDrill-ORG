e36afd0550b4f60f4e915d8797bd12da6923b089
KaiAdrian <thriller30@gmail.com>
2026-04-23 18:43:51 +0200

    ?:^... new regex const +add

--- regexSpamFilter/script.js
+++ regexSpamFilter/script.js
@@ -4,9 +4,10 @@ const checkMessageButton = document.getElementById("check-message-btn");
 
 const helpRegex = /please help|assist me/i; //i pomeni 'ignore caseSensitive'
 const dollarRegex = /[0-9]+\s*(?:hundred|thousand|million|billion)?\s+dollars/i;
-const freeRegex = /\sfr[e3][e3] m[o0]n[e3]y\s/i;
+const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
+const stockRegex = /stock alert/i;
 
-const denyList = [helpRegex, dollarRegex,freeRegex];
+const denyList = [helpRegex, dollarRegex, freeRegex,stockRegex];
 //const isSpam = (msg) => helpRegex.test(msg); //vrne true ali false
 // const isSpam = (msg) =>msg.match(helpRegex);///vrne truthy ali null
 const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
