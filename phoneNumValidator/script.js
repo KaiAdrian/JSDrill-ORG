5555814a9a5909c6c48c5c6d045e53fdfb627a7b
KaiAdrian <thriller30@gmail.com>
2026-04-27 11:43:51 +0200

    basic setup no logic yet

--- phoneNumValidator/script.js
+++ phoneNumValidator/script.js
@@ -1,1 +1,22 @@
-/////init
+const input = document.getElementById("user-input");
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const result = document.getElementById("results-div");
+
+//pravilne oblike:
+// 1 555-555-5555
+// 1 (555) 555-5555
+// 1(555)555-5555
+// 1 555 555 5555
+// 5555555555
+// 555-555-5555
+// (555)555-5555
+
+checkBtn.addEventListener("click", () => {
+  if (input.value.trim() === "") {
+    alert("Please provide a phone number");
+  }
+});
+clearBtn.addEventListener("click", () => {
+  result.innerHTML = "";
+});
